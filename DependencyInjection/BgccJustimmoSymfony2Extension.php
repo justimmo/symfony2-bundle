dd911a7aff57aa8f0a4c9af0b9f88f6a96cdd1f6
viktroll <v.anga@bgcc.at>
2015-03-11 17:22:20 +0100

    rename dependecy injection file

---
+++ DependencyInjection/BgccJustimmoSymfony2Extension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bgcc\Justimmo\Symfony2Bundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class BgccJustimmoSymfony2Extension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('bgcc_justimmo_symfony2.username', $config['username']);
+        $container->setParameter('bgcc_justimmo_symfony2.password', $config['password']);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
